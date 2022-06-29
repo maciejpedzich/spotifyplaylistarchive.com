d93f77ca699a334b6a9512f556f7260ce685dcdd
maciejpedzich <contact@maciejpedzi.ch>
2022-06-29 09:22:55 +0200

    Set up ESLint and PrimeVue

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    camelcase: 0,
+    'prettier/prettier': 2
+  }
+};
---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+import { defineNuxtConfig } from 'nuxt';
+import eslintPlugin from 'vite-plugin-eslint';
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  build: {
+    transpile: ['primevue']
+  },
+  css: [
+    'primevue/resources/primevue.min.css',
+    'primevue/resources/themes/arya-green/theme.css',
+    'primeflex/primeflex.min.css',
+    'primeicons/primeicons.css',
+    '~~/assets/base.css'
+  ],
+  vite: {
+    plugins: [eslintPlugin()]
+  }
+});
