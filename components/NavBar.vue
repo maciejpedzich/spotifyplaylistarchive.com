3725b558e44d660e0100796ac5fc7d1c20fdfde4
maciejpedzich <contact@maciejpedzi.ch>
2022-06-29 09:50:29 +0200

    Create a page navigation bar

---
+++ components/NavBar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import Menubar from 'primevue/menubar';
+
+interface MenuItem {
+  label: string;
+  icon: string;
+  to?: string;
+  visible?: boolean;
+  command?(): unknown;
+}
+
+const items = computed<MenuItem[]>(() => [
+  {
+    label: 'Home',
+    icon: 'pi pi-home',
+    to: '/'
+  },
+  {
+    label: 'About',
+    icon: 'pi pi-info-circle',
+    to: '/about'
+  }
+]);
+</script>
+
+<template>
+  <Menubar
+    :model="items"
+    class="fixed z-5 top-0 w-full justify-content-between md:justify-content-start"
+  >
+    <template #start>
+      <strong class="px-2 md:px-5">Spotify Playlist Archive</strong>
+    </template>
+  </Menubar>
+</template>
