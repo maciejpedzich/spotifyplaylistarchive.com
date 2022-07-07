ad05f4a35d7f67cad268b79bf3dbe666c679dbf1
maciejpedzich <contact@maciejpedzi.ch>
2022-07-07 08:44:49 +0200

    Increase vertical margin of track retention header

--- pages/playlists/[playlistId]/stats.vue
+++ pages/playlists/[playlistId]/stats.vue
@@ -2,7 +2,7 @@
   <div class="w-full text-lg text-center">
     <h2 class="mt-0 mb-3">Number of followers</h2>
     <StatsFollowerGrowth />
-    <h2 class="mt-4 mb-2">Track retention</h2>
+    <h2 class="mt-5 mb-3">Track retention</h2>
     <StatsTrackRetention />
   </div>
 </template>
