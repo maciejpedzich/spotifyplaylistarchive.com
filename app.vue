e54943323a4ca1068ffb98ea9276b170cfe53cde
maciejpedzich <contact@maciejpedzi.ch>
2022-07-13 20:03:12 +0200

    Add simple meta tag config

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import Toast from 'primevue/toast';
+
+useHead({
+  title: 'Spotify Playlist Archive',
+  meta: [
+    {
+      name: 'description',
+      content: 'Browse past versions of thousands of playlists saved over time'
+    }
+  ]
+});
 </script>
 
 <template>
