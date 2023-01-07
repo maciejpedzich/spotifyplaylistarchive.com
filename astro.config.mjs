bd746ba82ee5031562895b097d390714825d7de6
Maciej Pędzich <dnalop.me@gmail.com>
2023-01-07 14:56:19 +0100

    Install Vue.js integration for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
-  integrations: [tailwind()]
+  integrations: [tailwind(), vue()]
 });
