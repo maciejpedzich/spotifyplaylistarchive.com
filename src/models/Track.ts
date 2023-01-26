c6febffe38c4079cee033c899a2ac9252947deb3
Maciej Pędzich <dnalop.me@gmail.com>
2023-01-26 15:22:12 +0100

    Reintroduce Track and User models

---
+++ src/models/Track.ts
@@ -0,0 +1,18 @@
+import type { User } from './User';
+
+export interface Track {
+  added_at: string;
+  album: {
+    name: string;
+    url: string;
+  };
+  artists: User[];
+  duration_ms: number;
+  name: string;
+  url: string;
+  date_added: string;
+  date_added_asterisk: boolean;
+  date_removed: string | null;
+  position?: string;
+  retention?: number;
+}
